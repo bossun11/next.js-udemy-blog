eab906f554218f61226235f55a1e76dc61f9a76c
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-11-29 17:01:05 +0900

    Style: 全体のレイアウトを調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./Header";
 
 export const metadata: Metadata = {
   title: "Next.js Blog",
@@ -14,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className="container mx-auto bg-slate-700 text-slate-50">
+        <Header />
+        {children}
+        <footer>Footer</footer>
+      </body>
     </html>
   );
 }
