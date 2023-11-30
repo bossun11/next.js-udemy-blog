2843c8f4cbcfca4b24d944c495a7913a63820b9f
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-11-30 13:57:43 +0900

    Add: ローディングページを作成

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="h-16 w-16 border-t-4 border-orange-500 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/blogApi.ts
+++ src/blogApi.ts
@@ -7,6 +7,9 @@ export const getAllArticles = async (): Promise<Article[]> => {
     throw new Error("Network response was not ok");
   }
 
+  // 意図的に遅延を発生させる
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+
   const articles = await res.json();
   return articles;
 };
