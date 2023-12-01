0ae26d15abd2bc43440088fefe7e4dd0e3392bf6
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-12-01 14:48:48 +0900

    Add: 新規ブログを投稿できる機能を追加

--- src/blogApi.ts
+++ src/blogApi.ts
@@ -33,3 +33,25 @@ export const getDetailArticle = async (id: string): Promise<Article> => {
   const article = await res.json();
   return article;
 };
+
+export const createArticle = async (
+  id: string,
+  title: string,
+  content: string
+): Promise<Article> => {
+  const currentDateTime = new Date().toISOString();
+  const res = await fetch("http://localhost:3001/posts", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ id, title, content, createdAt: currentDateTime }),
+  });
+
+  if (!res.ok) {
+    throw new Error("Network response was not ok");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const newArticle = await res.json();
+  return newArticle;
+};
