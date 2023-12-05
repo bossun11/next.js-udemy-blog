08da89a9ee80206b5e354772a4b4a405854f6b22
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-12-05 13:35:14 +0900

    Change: ブログ記事削除用APIをSupabaseへリクエストを送るように変更

--- src/pages/api/[id].ts
+++ src/pages/api/[id].ts
@@ -7,18 +7,33 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { id } = req.query;
-  const { data, error } = await supabase
-    .from("posts")
-    .select("*")
-    .eq("id", id)
-    .single();
 
-  if (error) {
-    res.status(500).json({ error: error.message });
-  }
+  switch (req.method) {
+    case "GET":
+      const { data, error } = await supabase
+        .from("posts")
+        .select("*")
+        .eq("id", id)
+        .single();
 
-  if (!data) {
-    notFound();
+      if (error) {
+        res.status(500).json({ error: error.message });
+      }
+
+      if (!data) {
+        notFound();
+      }
+      return res.status(200).json(data);
+
+    case "DELETE":
+      const { error: deleteError } = await supabase
+        .from("posts")
+        .delete()
+        .eq("id", id);
+
+      if (deleteError) {
+        res.status(500).json({ error: deleteError.message });
+      }
+      return res.status(200).json({ message: "削除に成功しました。" });
   }
-  return res.status(200).json(data);
 }
